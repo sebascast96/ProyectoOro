c65ab8e14ff3af050b9f40661dfec5e67ebc5c30
sebascast96 <sebas-_-sebastian@hotmail.com>
2021-06-13 18:46:22 -0500

    first commit

---
+++ database/migrations/2021_06_13_204150_create_clients_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('rs');
+            $table->string('commercial_name');
+            $table->string('rfc');
+            $table->string('curp');
+            $table->string('address');
+            $table->date('birthdate');
+            $table->string('email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
---
+++ resources/views/clients/index.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content') 
+
+<div class="py-12">
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">Agregar Cliente</div>
+    
+                    <div class="card-body">
+                        @if (session()->has('message'))
+                        <div class="alert alert-primary" role="alert">
+                            {{session()->get('message') }}
+                          </div>
+                        @endif
+                        <table class="table">
+                            <thead>
+                              <tr>
+                                <th scope="col">Id</th>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Razon Social</th>
+                                <th scope="col">Nombre comercial</th>
+                                <th scope="col">Correo</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($clients as $client)
+                              <tr>
+                                <th scope="row">{{$client->id}}</th>
+                                <td>{{$client->name}}</td>
+                                <td>{{$client->rs}}</td>
+                                <td>{{$client->commercial_name}}</td>
+                                <td>{{$client->email}}</td>
+                              </tr>
+                            @endforeach
+                            </tbody>
+                          </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</div>
+
+@endsection
