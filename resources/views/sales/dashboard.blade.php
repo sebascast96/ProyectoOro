b3ee88202e64b82f32ee48e807e2d964fc02ede0
sebascast96 <sebas-_-sebastian@hotmail.com>
2021-08-09 21:12:23 -0500

    .

--- resources/views/sales/dashboard.blade.php
+++ resources/views/sales/dashboard.blade.php
@@ -2,76 +2,103 @@
 
 @section('content')
 
-<div class="py-12">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">Ventas
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            <label class="col-3 text-center">Fecha:</label>
-                            <h5 class="col-3">{{ date('d-m-Y') }}</h5>
-                            <label class="col-3 text-center">Vendedor:</label>
-                            <h5 class="col-3">{{ Auth::user()->name }}</h5>
+    <div class="py-12">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">Ventas
                         </div>
-
-                        <div class="row">
-                            <label class="col-3 text-center">Cliente:</label>
-                            <select class="select2client col-5" name="clients"></select>
-                            <a class="btn btn-success col-1">
-                                <span>
-                                    <i class="fas fa-user-plus"></i>
-                                </span></a>
-                        </div>
-
-
-                        @foreach ($products as $product)
-                            <div class="card" class="col-md-4">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{$product->name}}</h5>
-                                    <p class="card-text">{{$product->description}}</p>
-                                    <p class="card-text">Precio unitario: {{$product->price_perunit}}</p>
-                                    Cantidad:<button onclick="adding({{$product->id}})" class="btn btn-danger">-</button><input id="{{$product->id}}" type="text"><button  class="btn btn-success">+</button>
-                                 </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <label class="col-3 text-center">Fecha:</label>
+                                <h5 class="col-3">{{ date('d-m-Y') }}</h5>
+                                <label class="col-3 text-center">Vendedor:</label>
+                                <h5 class="col-3">{{ Auth::user()->name }}</h5>
                             </div>
-                        @endforeach
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button class="btn btn-primary">Finalizar venta</button>
-                        </div>
 
+                            <div class="row">
+                                <label class="col-3 text-center">Cliente:</label>
+                                <select class="select2client col-5" name="clients"></select>
+                                @can('create-clients')
+                                    <a class="btn btn-success col-1" href="{{ route('clients.create') }}">
+                                        <span>
+                                            <i class="fas fa-user-plus"></i>
+                                        </span>
+                                    </a>
+                                @endcan
+                            </div>
+
+
+                            @foreach ($products as $product)
+                                <div class="card" class="col-4">
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $product->name }}</h5>
+                                        <p class="card-text">{{ $product->description }}</p>
+                                        <p class="card-text" id="price {{ $product->id }}">Precio unitario:
+                                            {{ $product->price_perunit }}</p>
+                                        <p class="card/text">En existencia: {{ $product->amount }}</p>
+                                        <p>Cantidad:</p>
+                                        <input type="number"
+                                            onkeyup="calc({{ $product->id }},{{ $product->price_perunit }})"
+                                            id="text {{ $product->id }}" class="form-control">
+                                        <p class="text-right" id="total {{ $product->id }}">Total: </p>
+                                    </div>
+                                </div>
+                            @endforeach
+                            <p class="text-right" id="total">Subtotal: $0.00</p>
+                            <p class="text-right">Iva: $0.00</p>
+                            <p class="text-right">Total: $0.00</p>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <button class="btn btn-primary">Finalizar venta</button>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<script>
-    $(document).ready(function() {
-        function adding($id) {
-            console.log(document.getElementById($id).innerHTML);
+    <script>
+        function calc(id, price) {
+            if (document.getElementById("text " + id).value == "Nan") {
+                document.getElementById("total " + id).innerHTML = "Total:  "
+            } else {
+                document.getElementById("total " + id).innerHTML = "Total: " +
+                    parseInt(document.getElementById("text " + id).value) * price
+            }
+
+
+
         };
-      function format(item) { return item.name; };
+        $(document).ready(function() {
 
-        $select2client = $('.select2client').select2({
-            placeholder: "¿Que cliente esta comprando?",
-            ajax:{
-                url:'/fill-clients',
-                dataType: 'json',
-                type:'GET',
-                processResults: function (data) {
-                    return {
-                        results: $.map(data, function(obj) {
-                            return { id: obj.id, text: obj.name };
-                        })
-                    };
-                }
-            },
+
+            function format(item) {
+                return item.name;
+            };
+
+            $select2client = $('.select2client').select2({
+                placeholder: "¿Que cliente esta comprando?",
+                ajax: {
+                    url: '/fill-clients',
+                    dataType: 'json',
+                    type: 'GET',
+                    processResults: function(data) {
+                        return {
+                            results: $.map(data, function(obj) {
+                                return {
+                                    id: obj.id,
+                                    text: obj.name
+                                };
+                            })
+                        };
+                    }
+                },
+            });
+
+
+
         });
-
-
-
-    });
-</script>
+    </script>
 @endsection
